2966a676ac4d6f4a23c8fea779b965c469a607f6
ahmed-naeem2 <hnym57@gmail.com>
2024-10-12 11:45:15 -0700

    create project controller

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,69 @@
+﻿using AonFreelancing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AonFreelancing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private static List<Project> _projects = new List<Project>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            return Ok(_projects);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Project project)
+        {
+            _projects.Add(project);
+            return CreatedAtAction("Create", new { Id = project.Id }, _projects);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetFreelancer(int id)
+        {
+            Project pr = _projects.FirstOrDefault(pr => pr.Id == id);
+
+            if (pr == null)
+            {
+                return NotFound("The resoucre is not found!");
+            }
+
+            return Ok(pr);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Project pr = _projects.FirstOrDefault(pr => pr.Id == id);
+            if (pr != null)
+            {
+                _projects.Remove(pr);
+                return Ok("Deleted");
+
+            }
+
+            return NotFound();
+        }
+        [HttpPost("{Id}")]
+         public IActionResult Update(int id)
+         {
+
+             Project pr = _projects.FirstOrDefault(p => p.Id == id);
+             if (pr != null) {
+
+                _projects.Add(pr);
+
+
+             }
+         }
+        
+
+
+    }
+}
+
---
+++ Models/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace AonFreelancing.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }    
+        public string Name { get; set; }
+
+
+    }
+}
